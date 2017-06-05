ac32b2bb10126998109f829c5686a30424fe988b
Hayes Capers <capersj5@yahoo.com>
2017-06-05 18:36:57 -0400

    basic queries to sql

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config/config');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host: config.host,
+	user: config.userName,
+	password: config.password,
+	database: config.database,
+});
+
+connection.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var message = req.query.msg;
+	if (message == 'added'){
+		message = 'Your task was added';
+	}
+	var selectQuery = "SELECT * FROM tasks";
+	connection.query(selectQuery, (error,results)=>{
+		console.log(results)
+		res.render('index', {
+			message: message,
+			taskArray: results
+		});
+	});
+ 	// res.render('index', { message: message });
 });
+
+router.post('/addItem',(req,res)=>{
+	// res.json(req.body)
+	var newTask = req.body.newTask;
+	var dueDate = req.body.newTaskDate;
+	var insertQuery = `INSERT INTO tasks (taskName, taskDate) VALUES ('${newTask}','${dueDate}')`;
+
+	connection.query(insertQuery,(error,results)=>{
+		if(error) throw error;
+		res.redirect('/?msg=added');
+	});
+});
+
+router.get('/delete/:id', (req,res)=>{
+	var idToDelete = req.params.id;
+	var deleteQuery = `DELETE FROM tasks WHERE id = ${idToDelete}`;
+	// res.send(idToDelete);
+	connection.query(deleteQuery, (error, results)=>{
+		res.redirect('/?msg=delete');
+	})
+})
 
 module.exports = router;
